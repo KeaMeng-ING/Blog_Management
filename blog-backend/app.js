b9ab01fcbd5b2dece4fbdab0eb7120ca7966a704
keameng <shopilink.kh@gmail.com>
2026-05-17 15:48:39 +0700

    Merge Blog-Admin to Blog Backend

--- blog-backend/app.js
+++ blog-backend/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8080;
-const cors = require("cors"); // Import CORS
+const cors = require("cors");
 const postRoutes = require("./routes/postRoutes");
 const userRoutes = require("./routes/userRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 
-app.use(cors()); // Enable CORS
-app.use(express.json({ limit: "10mb" })); // Adjust the limit as needed
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/test", (req, res) => {
   res.send("Hello World!");
@@ -20,6 +23,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/admin", adminRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port https://localhost:${port}`);
